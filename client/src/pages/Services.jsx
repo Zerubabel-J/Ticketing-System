e20ece97e97c9c2277a76e2e58818eab4c3e12b7
Zerubabel <zerucodes@gmail.com>
2025-03-05 00:33:57 +0300

    Services added

---
+++ client/src/pages/Services.jsx
@@ -0,0 +1,19 @@
+export function Services() {
+  return (
+    <div className="min-h-screen flex flex-col items-center  p-6">
+      <div className="max-w-3xl text-center space-y-6">
+        <h2 className="text-4xl font-bold">About Our Service</h2>
+        <p className="text-lg">
+          Our Ticketing System helps businesses and individuals efficiently
+          track and manage their tickets. Whether you're handling support
+          issues, event tickets, or internal requests, our system streamlines
+          the process.
+        </p>
+        <p className="text-lg">
+          Join us today and experience seamless ticket management with an
+          intuitive and responsive interface.
+        </p>
+      </div>
+    </div>
+  );
+}
